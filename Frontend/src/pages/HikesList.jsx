8f490f7debe67c9b1b03eafc7916fad20ca9a3c9
Gal Lankri <gal.lankri2@gmail.com>
2025-06-05 15:05:00 +0300

    adding card list of the trips from the db

--- Frontend/src/pages/HikesList.jsx
+++ Frontend/src/pages/HikesList.jsx
@@ -1,22 +1,22 @@
-import {useEffect, useState} from 'react'
-import { hikeService } from '../services/hikeService'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { hikeService } from "../services/hikeService";
 
 function HikesList() {
-  const [hikes, setHikes] = useState([])
+  const [hikes, setHikes] = useState([]);
 
   // Fetch hikes data using the hikeService
   useEffect(() => {
     const fetchHikes = async () => {
       try {
-        setHikes(await hikeService.getHikes())
-        console.log('Fetched hikes:', hikes)
+        setHikes(await hikeService.getHikes());
+        console.log("Fetched hikes:", hikes);
       } catch (error) {
-        console.error('Error fetching hikes:', error)
+        console.error("Error fetching hikes:", error);
       }
-    }
-    fetchHikes()
-  }, [])
-
+    };
+    fetchHikes();
+  }, []);
 
   return (
     <div className="bg-primary flex flex-1 flex-col w-full p-10">
@@ -24,25 +24,28 @@ function HikesList() {
         <h1 className="font-bold text-4xl">Hikes List</h1>
         <p>Explore our collection of amazing hikes.</p>
       </header>
-      <main>
         <section className="flex flex-col items-center gap-4">
           <h2>Available Hikes</h2>
-          <ul className="list-disc list-inside">
+          <ul className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {hikes.map((hike) => (
-              <li key={hike.id} className="text-lg">
-                <a href={`/hikes/${hike.id}`} className="text-blue-500 hover:underline">
-                  {hike.name}
-                </a>
-              </li>
+                <li
+                  key={hike.id}
+                >
+              <Link to={`/hikes/${hike.id}`} className="text-lg border-2 block border-gray-300 p-4 rounded-lg w-full max-w-md hover:border-green-500 hover:shadow-md transition-all duration-200">
+                  <h3 className="font-semibold">{hike.name}</h3>
+                  <p className="text-sm text-gray-500">
+                    {hike.location} · {hike.difficulty} · {hike.length_km} km
+                  </p>
+              </Link>
+                </li>
             ))}
           </ul>
           {hikes.length === 0 && (
             <p className="text-gray-500">No hikes available at the moment.</p>
           )}
         </section>
-      </main>
     </div>
-  )
+  );
 }
 
-export default HikesList
+export default HikesList;
